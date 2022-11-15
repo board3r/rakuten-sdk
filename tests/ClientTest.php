a373597eefe3bfee30c5b61fc088080252708641
Maxime <maxime@nap-agency.com>
2022-11-15 17:51:02 +0100

    Init the project

---
+++ tests/ClientTest.php
@@ -0,0 +1,100 @@
+<?php
+/*
+ * Copyright (c) 2007-$year NAP Agency
+ *
+ *
+ */
+
+namespace Tests;
+
+use RakutenSDK\Client;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+
+    public function testGetGenericReport()
+    {
+
+    }
+
+    public function testGetAccount()
+    {
+
+    }
+
+    public function testGetProductTypeTemplate()
+    {
+
+    }
+
+    public function testSetSandbox()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetShippingTypes()
+    {
+
+    }
+
+    public function testGetListing()
+    {
+
+    }
+
+    public function testGetSandbox()
+    {
+
+    }
+
+    public function testGetExport()
+    {
+
+    }
+
+    public function testSetToken()
+    {
+
+    }
+
+    public function testGetCategories()
+    {
+
+    }
+
+    public function testGetProductTypes()
+    {
+
+    }
+
+    public function testSetAccount()
+    {
+
+    }
+
+    public function testGetNewSales()
+    {
+
+    }
+
+    public function testGetBaseUrl()
+    {
+
+    }
+
+    public function testSendGenericFile()
+    {
+
+    }
+
+    public function testGetToken()
+    {
+
+    }
+}
